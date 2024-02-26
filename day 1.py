2611e8dd61ade3477aba7b47ef98851b815088f7
eshaan <92547104+eshnd@users.noreply.github.com>
2024-02-26 14:44:07 -0600

    Create day 1.py

---
+++ day 1.py
@@ -0,0 +1,13 @@
+print ("Day 1\nHW assigned 2/21 (A) & 2/22 (B): Bentley 31 & 33 Russian Empire & Revolution Reading & G.O.
+Due Feb 23 by 8:59am Points None
+Complete this G.O.Links to an external site.as you read about Russia in Bentley Ch. 31 & 33 via ConnectED in Canvas. Follow the page numbers and headings carefully as you are not reading the entire chapters. 
+
+ 
+
+Here is a copy of the reading if you cannot access ConnectEdLinks to an external site.. 
+
+ 
+
+ 
+
+")
