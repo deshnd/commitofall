8cca3dae919e2877617a9c5d7f30f70de0df82ae
eshaan <92547104+eshnd@users.noreply.github.com>
2024-02-27 15:17:32 -0600

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,242 @@
+print("""
+
+Type / to search
+
+Top Repositories
+Find a repository…
+eshnd.github.io
+eshnd/eshnd.github.io
+belldesign101
+eshnd/belldesign101
+downlink
+eshnd/downlink
+mbyms-dhtml.github.io
+mbyms-dhtml/mbyms-dhtml.github.io
+NO
+eshnd/NO
+imageforsai
+eshnd/imageforsai
+commitofall
+eshnd/commitofall
+Recent activity
+When you take actions across GitHub, we’ll provide links to that activity here.
+
+Dashboard
+Home
+Send feedback
+Updates to your homepage feed
+We've combined the power of the Following feed with the For you feed so there’s one place to discover content on GitHub. There’s improved filtering so you can customize your feed exactly how you like it, and a shiny new visual design. ✨
+
+Learn more
+@Blue-Robin-Taken profile
+Blue-Robin-Taken starred 
+a repository
+last week
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/MorallyQuestionable
+For the qubitx hacks by ycw hackathon
+ CSS 2
+@Blue-Robin-Taken profile
+Blue-Robin-Taken starred 
+a repository
+last week
+@matthewzring profile
+matthewzring/CyberScores
+A Discord bot for interaction with the CyberPatriot scoreboard
+ C# 65
+Trending repositories
+·
+See more
+@WongKinYiu profile
+WongKinYiu/yolov9
+Implementation of paper - YOLOv9: Learning What You Want to Learn Using Programmable Gradient Information
+ Python 4.9k
+@public-apis profile
+public-apis/public-apis
+A collective list of free APIs
+ Python 282k
+@Blue-Robin-Taken profile
+Blue-Robin-Taken created a repository
+last week
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/MorallyQuestionable
+For the qubitx hacks by ycw hackathon
+ CSS 2
+@Blue-Robin-Taken profile
+Blue-Robin-Taken created a repository
+2 weeks ago
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/WordleClone
+@Blue-Robin-Taken profile
+Blue-Robin-Taken followed 
+2 weeks ago
+@Jack-Slayton profile
+Jack-Slayton Jack-Slayton
+
+Currently learning Python and Java.
+2 repositories 5 followers
+
+@Blue-Robin-Taken profile
+Blue-Robin-Taken created a repository
+2 weeks ago
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/Funny-thing-for-CS-club-Competition-Valentines-day
+ HTML
+@Blue-Robin-Taken profile
+Blue-Robin-Taken followed 
+3 weeks ago
+@stylo-codes-stuff profile
+Stylo stylo-codes-stuff
+
+sophmore in highschool who likes web development and software development. Looking to become a full stack dev one day!
+25 repositories 5 followers
+
+@Blue-Robin-Taken profile
+feed-public
+Blue-Robin-Taken made this repository public
+3 weeks ago
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/nestling-bot
+ Python 1
+@Blue-Robin-Taken profile
+Blue-Robin-Taken starred 
+a repository
+3 weeks ago
+@anuraghazra profile
+anuraghazra/github-readme-stats
+⚡ Dynamically generated stats for your github readmes
+ JavaScript 63.4k
+@Blue-Robin-Taken profile
+Blue-Robin-Taken followed 
+2 users
+3 weeks ago
+@DragonflyRobotics profile
+Krishna Shah DragonflyRobotics
+
+I am a robotics hobbyist and founder of DeepShift Labs. I love contributing and innovating.
+27 repositories 23 followers
+
+@KingUndeadCodes profile
+KingUndeadCodes
+
+Professional Stack Overflow code copier.
+9 repositories 9 followers
+
+@Blue-Robin-Taken profile
+Blue-Robin-Taken starred 
+a repository
+3 weeks ago
+@FujiwaraChoki profile
+FujiwaraChoki/MoneyPrinter
+Automate Creation of YouTube Shorts using MoviePy.
+ Python 8.1k
+@Blue-Robin-Taken profile
+Blue-Robin-Taken followed 
+3 weeks ago
+@SakethSripada profile
+Saketh Sripada SakethSripada
+
+CS student interested in web development, application development, and artificial intelligence
+10 repositories 2 followers
+
+@Blue-Robin-Taken profile
+Blue-Robin-Taken created a repository
+3 weeks ago
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/Productivity-Bot
+ Python
+@Jack-Slayton profile
+Jack-Slayton started following 
+you
+3 weeks ago
+@Jack-Slayton profile
+Jack-Slayton Jack-Slayton
+
+Currently learning Python and Java.
+2 repositories 5 followers
+
+@Blue-Robin-Taken profile
+Blue-Robin-Taken starred 
+a repository
+3 weeks ago
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/MerlinLiason
+Communication tool for SSH to communicate with one another using various endpoints and JavaScript
+ Python 2
+@Blue-Robin-Taken profile
+Blue-Robin-Taken followed 
+3 weeks ago
+@DBenYaakov profile
+DBenYaakov
+
+3 repositories 12 followers
+
+@Blue-Robin-Taken profile
+Blue-Robin-Taken followed 
+3 weeks ago
+@Pycord-Development profile
+Pycord Development Pycord-Development
+
+A team of developers working on maintaining pycord
+11 repositories 91 followers
+
+@Blue-Robin-Taken profile
+Blue-Robin-Taken followed 
+3 weeks ago
+@plun1331 profile
+plun1331 plun1331
+
+i code stuff and do a lot of water sports certified lifeguard, which is pretty cool
+34 repositories 24 followers
+
+@Blue-Robin-Taken profile
+Blue-Robin-Taken created 
+2 repositories
+3 weeks ago
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/MerlinLiason
+Communication tool for SSH to communicate with one another using various endpoints and JavaScript
+ Python 2
+@Blue-Robin-Taken profile
+Blue-Robin-Taken/Computer-Science-Portfolio
+The portfolio for my Computer Science Class in high school
+ Shell
+More
+Footer
+© 2024 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact
+Manage cookies
+Do not share my personal information
+GitHub logo
+What does it mean for a technology to go mainstream? Discover how AI is changing the developer experience.
+
+Latest changes
+50 minutes ago
+Copilot – February 27th update
+1 hour ago
+Repository Rules – configure merge queue rule – public beta
+4 hours ago
+GitHub Copilot Enterprise is now generally available
+4 days ago
+Secret scanning supports user namespace repositories for Enterprise Managed Users
+View changelog →
+Explore repositories
+@gregsdennis profile gregsdennis / json-everything
+System.Text.Json-based support for all of your JSON needs.
+
+ 673  C#
+@outline profile outline / outline
+The fastest knowledge base for growing teams. Beautiful, realtime collaborative, feature packed, and markdown compatible.
+
+ 23k  TypeScript
+@outline profile outline / outline
+The fastest knowledge base for growing teams. Beautiful, realtime collaborative, feature packed, and markdown compatible.
+
+ 23k  TypeScript
+Explore more →""")
